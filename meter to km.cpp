ea66ed39fbfaec59a8a9a09395c5222a34449db4
Mohit Bhade <128077306+sirmohit@users.noreply.github.com>
2024-03-11 19:40:36 +0530

    Add files via upload

---
+++ meter to km.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+ int main()
+{
+   float meter,km;
+   cout<<"Enter value meter:"<<meter;
+   cin>>meter;
+   km=meter/1000;
+   cout<<meter<<"meter"<<"="<<km<<"km";
+   return 0;
+}
