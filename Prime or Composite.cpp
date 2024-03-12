07cc47fecf00637ee06b9132f20e86f7630b0983
Mohit Bhade <128077306+sirmohit@users.noreply.github.com>
2024-03-12 19:08:18 +0530

    Add files via upload

---
+++ Prime or Composite.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+ int main()
+{
+   float meter,km;
+   cout<<"Enter value meter:"<<meter;
+   cin>>meter;
+   km=meter/1000;
+   cout<<meter<<"meter"<<"="<<km<<"km";
+}
