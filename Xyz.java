22051b11ffa1c968b0db8f31157577bb42b94aac
Mohit Bhade <128077306+sirmohit@users.noreply.github.com>
2024-03-24 12:22:48 +0530

    Add files via upload

---
+++ Xyz.java
@@ -0,0 +1,15 @@
+package OOPs;
+
+public class Xyz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		do 
+		  
+		 while(true)
+		  System.out.println("ewe");
+		while(false);
+
+	}
+
+}
